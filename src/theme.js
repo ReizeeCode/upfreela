dbb391ef17b1aa63efbe3a3e2f6307ecb2873d9c
vini <vribeirobarreto@gmail.com>
2022-10-10 21:28:58 -0300

    Estrutura inicial + material.ui

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from '@material-ui/core/styles'
+
+// cria tema personalizado
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#000000',
+        }
+    }
+})
+
+
+export default theme
