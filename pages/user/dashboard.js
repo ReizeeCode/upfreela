57116ad5674e26155784378c059b859b3b459a5e
EvaldoAssis <junior29613372@gmail.com>
2022-11-12 17:28:38 -0300

    Dashboard - listando anúncios do usuário

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -6,8 +6,13 @@ import {
 } from '@material-ui/core'
 
 import { makeStyles } from '@material-ui/core/styles'
+import { getSession } from 'next-auth/client'
+
+import dbConnect from '../../src/utils/dbConnect'
+import ServicesModel from '../../src/models/services'
 import TemplateDefault from '../../src/templates/Default'
 import Card from '../../src/components/Card'
+
 
 // cria css para a página dashboard 
 const useStyles = makeStyles((theme) => ({
@@ -18,13 +23,14 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-const Home = () => {
+const Home = ( { services } ) => {
   const classes = useStyles()
+
+  console.log(services)
 
   return (
     <TemplateDefault>
-      {/* deixa a tela em tamanho 'small' */}
-      <Container maxWidth="sm"> 
+      <Container maxWidth="sm">
         <Typography component="h1" variant="h2" align="center">
           Meus Anúncios
         </Typography>
@@ -33,60 +39,28 @@ const Home = () => {
         </Button>
       </Container>
       <Container maxWidth="md">
-        <Grid container spacing={4}> {/* espaçamento entre os containers */}
-          <Grid item xs={12} sm={6} md={4}> {/* responsividade entre telas */}
-            <Card 
-                image={'https://source.unsplash.com/random'}
-                title="Serviço X"
-                subtitle="5 dias"
-                actions={
-                  <>
-                    <Button size="small" color="primary">
-                      Editar
-                    </Button>
-                    <Button size="small" color="primary">
-                      Inativar
-                    </Button>
-                  </>
-                }
-            />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={4}> {/* responsividade entre telas */}
-            <Card 
-                image={'https://source.unsplash.com/random'}
-                title="Serviço X"
-                subtitle="5 dias"
-                actions={
-                  <>
-                    <Button size="small" color="primary">
-                      Editar
-                    </Button>
-                    <Button size="small" color="primary">
-                      Inativar
-                    </Button>
-                  </>
-                }
-            />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={4}> {/* responsividade entre telas */}
-            <Card 
-                image={'https://source.unsplash.com/random'}
-                title="Serviço X"
-                subtitle="5 dias"
-                actions={
-                  <>
-                    <Button size="small" color="primary">
-                      Editar
-                    </Button>
-                    <Button size="small" color="primary">
-                      Inativar
-                    </Button>
-                  </>
-                }
-            />
-          </Grid>
+        <Grid container spacing={4}>
+          {
+            services.map(service => (
+              <Grid key={service._id} item xs={12} sm={6} md={4}>
+                <Card
+                  image={`/uploads/${service.files[0].name}`} 
+                  title={service.title}
+                  subtitle="5 dias"
+                  actions={
+                    <>
+                      <Button size="small" color="primary">
+                        Editar
+                      </Button>
+                      <Button size="small" color="primary">
+                        Inativar
+                      </Button>
+                    </>
+                  }
+                />
+              </Grid>
+            ))
+          }
 
         </Grid>
       </Container>
@@ -96,5 +70,19 @@ const Home = () => {
 
 // autenticação obrigatória
 Home.requireAuth = true
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req })
+  //conectando no banco
+  await dbConnect()
+
+  const services = await ServicesModel.find({ 'user.id': session.userId })
+
+  return {
+    props: {
+      services: JSON.parse(JSON.stringify(services)),
+    }
+  }
+}
 
 export default Home
